5b486adb7d38fee7a041acc9da75041027ad1839
m4st3rP <31252555+m4st3rP@users.noreply.github.com>
2017-11-11 01:32:07 +0100

    Add files via upload

---
+++ palindromeFinder/PalindromeFinder.java
@@ -0,0 +1,25 @@
+package palindromeFinder;
+
+public class PalindromeFinder {
+  String text;
+  
+  public PalindromeFinder() {}
+  
+  public PalindromeFinder(String givenText) {
+    this.text = givenText;
+  }
+  
+  public String getText() {
+    return text;
+  }
+
+  public String getLongestPalindrome() {
+    //TODO Platzhalter
+    return "";
+  }
+  
+  public void setText(String givenText) {
+    this.text = givenText;
+  }
+  
+}
