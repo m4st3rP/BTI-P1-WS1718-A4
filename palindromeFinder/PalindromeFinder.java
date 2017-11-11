4efdd84e6030561ef48804e994d310618b9dd9a3
m4st3rP <31252555+m4st3rP@users.noreply.github.com>
2017-11-11 17:49:58 +0100

    Update PalindromeFinder.java

--- palindromeFinder/PalindromeFinder.java
+++ palindromeFinder/PalindromeFinder.java
@@ -2,24 +2,52 @@ package palindromeFinder;
 
 public class PalindromeFinder {
   String text;
-  
+
   public PalindromeFinder() {}
-  
+
   public PalindromeFinder(String givenText) {
     this.text = givenText;
   }
-  
+
   public String getText() {
     return text;
   }
 
   public String getLongestPalindrome() {
-    //TODO Platzhalter
-    return "";
+    char[] textAsArray = this.text.toCharArray();
+    String longestPalindrome = "";
+    int repetitionCounter = 1;
+    int a = 0;
+    int b = textAsArray.length - repetitionCounter;;
+
+    for (int i = 0; i<repetitionCounter; i++) {
+      a = 0;
+      b = textAsArray.length - repetitionCounter;
+      while (isPalindrome(a, b, textAsArray) == false || a<b) {
+        a++;
+        b++;
+      }
+      repetitionCounter++;
+    }
+    
+    //TODO entfernen
+    System.out.println(a + ", " + b);
+    for (int i = a; i <= b; i++) {
+      longestPalindrome = longestPalindrome + textAsArray[i];
+    }
+    return longestPalindrome;
   }
-  
+
   public void setText(String givenText) {
     this.text = givenText;
   }
-  
+
+  public boolean isPalindrome(int a, int b, char[] textAsArray) {
+    while ((a < b) && (Character.toLowerCase(textAsArray[a]) == Character.toLowerCase(textAsArray[b]))) {
+      a++;
+      b--;
+    }
+    return a >= b;
+  }
 }
+
