aaf2275a7a30ef5a2153104bfd0b47beeec58827
m4st3rP <31252555+m4st3rP@users.noreply.github.com>
2017-11-13 12:48:31 +0100

    Made it more OOP correct

--- bankAccount/CurrentAccount.java
+++ bankAccount/CurrentAccount.java
@@ -1,33 +1,20 @@
 package bankAccount;
 
 public class CurrentAccount extends BankAccount {
-  int fee;
+  private int fee;
 
   public CurrentAccount(String id, long startBalance, int fee) {
     super(id, startBalance);
     this.fee = fee;
   }
-
-  public long getAccountBalance() {
-    return this.balance;
-  }
   
   @Override
   public void withdraw(long amount) {
-    if (this.balance - (amount + this.fee) >= 0) {
-      this.balance = this.balance - (amount + this.fee);
-    } else {
-      System.out.println("The account balance is not allowed to go below 0!");
-    }
+      super.withdraw(amount + this.fee);
   }
 
-  // TODO mit String.format() überarbeiten?
   @Override
   public String toString() {
-    return "Account Type: "     + this.getClass().getSimpleName() +
-           "\nID: "             + this.id +
-           "\nBalance: "        + this.balance +
-           "\nWithdrawal Fee: " + this.fee +
-           "\n";
+    return super.toString() + "Withdrawal Fee: " + this.fee + "\n";
   }
 }
