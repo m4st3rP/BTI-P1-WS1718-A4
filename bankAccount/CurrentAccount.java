f7d71d9a203fd67182e25c9767403b4f237c03dc
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-11-15 15:36:04 +0100

    Update CurrentAccount.java

--- bankAccount/CurrentAccount.java
+++ bankAccount/CurrentAccount.java
@@ -1,34 +1,28 @@
 package bankAccount;
 
-/**
- * A subclass of bankAccount to creates objects of the type CurrentAccount.
- * 
- * @author Philipp Schwarz
- *
- */
 public class CurrentAccount extends BankAccount {
-  private int fee;
 
-  /**
-   * The constructor of a current account.
-   * 
-   * @param id The ID of the Current Account.
-   * @param startBalance The start balance of the Current Account.
-   * @param fee The withdrawal fees of the Current Account.
-   */
-  public CurrentAccount(String id, long startBalance, int fee) {
-    super(id, startBalance);
-    this.fee = fee;
-  }
+    private int fee;
 
-  // override so that fee gets deducted too
-  @Override
-  public void withdraw(long amount) {
-    super.withdraw(amount + this.fee);
-  }
+    
+    public CurrentAccount(String id, long cent, int fee) {
+        super(id, cent);
+        this.fee = fee;
+    }
 
-  @Override
-  public String toString() {
-    return super.toString() + String.format("Fee=%d]", fee);
-  }
+    
+    @Override
+    public void withdraw(long amount) {
+        super.withdraw(amount + fee);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "[<%s>: Fee=%d %s]",
+            CurrentAccount.class.getSimpleName(),
+            fee,
+            super.toString()
+        );
+    }
 }
