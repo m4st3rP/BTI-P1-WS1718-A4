fcf2e4d23e06b6fe5984c24de506b4a2a40e7fd6
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-11-15 01:51:54 +0100

    Added comments and redone toString

--- bankAccount/SavingsAccount.java
+++ bankAccount/SavingsAccount.java
@@ -1,20 +1,36 @@
 package bankAccount;
 
+/**
+ * A subclass of bankAccount to creates objects of the type SavingsAccount.
+ * 
+ * @author Philipp Schwarz
+ *
+ */
 public class SavingsAccount extends BankAccount {
   private int interestRate;
 
+  /**
+   * The constructor of a savings account.
+   * 
+   * @param id The ID of the savings account.
+   * @param startBalance The start balance of the savings account.
+   * @param interestRate The interest rate the account receives for each Interest rate it gets.
+   */
   public SavingsAccount(String id, long startBalance, int interestRate) {
     super(id, startBalance);
     this.interestRate = interestRate;
   }
 
+  /**
+   * To distribute the interest rate.
+   */
   public void giveInterest() {
     // Since long always round down this operation is always in benefit of the bank
-    deposit(getAccountBalance()*this.interestRate / 1000);
+    deposit(getAccountBalance() * this.interestRate / 1000);
   }
 
   @Override
   public String toString() {
-    return super.toString() + " Interest Rate: " + this.interestRate;
+    return super.toString() + String.format("Interest Rate=%d]", interestRate);
   }
 }
