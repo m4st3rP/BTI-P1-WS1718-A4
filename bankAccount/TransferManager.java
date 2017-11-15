0d39a2dbed9ccf8ca9d9af8ff6b46dac4296758f
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-11-15 01:51:08 +0100

    Added comments

--- bankAccount/TransferManager.java
+++ bankAccount/TransferManager.java
@@ -1,7 +1,20 @@
 package bankAccount;
 
+/**
+ * A class to create an object that makes it possible to transfer money from one account to the other.
+ * 
+ * @author Philipp Schwarz
+ *
+ */
 public class TransferManager {
 
+  /**
+   * The method of a transfer manager object that transfers money from one bank account to another.
+   * 
+   * @param sourceAccount The source bank account of the transaction.
+   * @param targetAccount The target bank account of the transaction.
+   * @param amount The amount getting transfered.
+   */
   public void transfer(CurrentAccount sourceAccount, CurrentAccount targetAccount, long amount) {
     long oldAmount = sourceAccount.getAccountBalance();
     sourceAccount.withdraw(amount);
