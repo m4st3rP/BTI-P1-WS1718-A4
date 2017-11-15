a8a0425e825b335d861683e5721965c93272d75a
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-11-15 01:52:21 +0100

    Added comments and redone toString()

--- bankAccount/CurrentAccount.java
+++ bankAccount/CurrentAccount.java
@@ -1,20 +1,34 @@
 package bankAccount;
 
+/**
+ * A subclass of bankAccount to creates objects of the type CurrentAccount.
+ * 
+ * @author Philipp Schwarz
+ *
+ */
 public class CurrentAccount extends BankAccount {
   private int fee;
 
+  /**
+   * The constructor of a current account.
+   * 
+   * @param id The ID of the Current Account.
+   * @param startBalance The start balance of the Current Account.
+   * @param fee The withdrawal fees of the Current Account.
+   */
   public CurrentAccount(String id, long startBalance, int fee) {
     super(id, startBalance);
     this.fee = fee;
   }
-  
+
+  // override so that fee gets deducted too
   @Override
   public void withdraw(long amount) {
-      super.withdraw(amount + this.fee);
+    super.withdraw(amount + this.fee);
   }
 
   @Override
   public String toString() {
-    return super.toString() + " Withdrawal Fee: " + this.fee;
+    return super.toString() + String.format("Fee=%d]", fee);
   }
 }
